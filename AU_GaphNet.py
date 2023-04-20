7cfb137f28062a593b9f8a89b2c24f5b5e64a62c
pixelhero98 <116365512+pixelhero98@users.noreply.github.com>
2023-04-20 14:54:31 +0100

    Update AU_GaphNet.py

--- AU_GaphNet.py
+++ AU_GaphNet.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 import torch_geometric.nn as tnn
 import torch.nn.functional as F
-
+from joint_emb import *
 
 class AU_Net(nn.Module):
 
-  def __init__(self, ins, hids0, hids1, hids2, outs, num_labels):
+  def __init__(self, ins, hids0, hids1, hids2, outs, num_labels, adj_dim):
 
     super(AU_Net, self).__init__()
 
@@ -16,11 +16,23 @@ class AU_Net(nn.Module):
     self.gcnconv2 = tnn.GCNConv(hids1, hids2)
     self.edgeconv2 = nn.Linear(hids2 + hids1 + hids0, hids2)
     self.edgeconv3 = nn.Linear(hids2, outs)
+    self.gdcconv = tnn.GDC(self_loop_weight=1,
+        normalization_in='sym',
+        normalization_out='col',
+        diffusion_kwargs=dict(method='coeff', alpha=0.05),
+        sparsification_kwargs=dict(method='topk', k=128, dim=0),
+        exact=True)
+    self.joint_emb = joint_gemb(ins, 2048, adj_dim)
     self.out = nn.Linear(outs, num_labels)
+    self.neighconv = nconv()
 
 
   def forward(self, x, edge_index, gx):
-
+    
+    zz = self.joint_emb(x, gx)
+    zz = self.gdcconv(zz)
+    x = self.neighconv(x, zz)
+  
     z = F.relu(self.edgeconv1(torch.concat((x, gx), dim=1)))
     z0 = self.dimreduc(z)
     z1 = F.relu(self.gcnconv1(z + gx, edge_index))
@@ -31,14 +43,7 @@ class AU_Net(nn.Module):
 
     return z
 
-  def reset_parameters(self):
-
-    self.edgeconv1.reset_parameters()
-    self.edgeonv2.reset_parameters()
-    self.gcnconv1.reset_parameters()
-    self.gcnconv2.reset_parameters()
-    self.edgeconv3.reset_parameters()
-    self.dimreduc.reset_parameters()
+ 
 
 
 
