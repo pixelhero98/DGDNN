64aec07e32984b85b62e7869efc88ea9baab9994
pixelhero98 <116365512+pixelhero98@users.noreply.github.com>
2023-11-09 09:25:13 +0000

    Update next_day_movement_prediction.py

--- Train_Eval/next_day_movement_prediction.py
+++ Train_Eval/next_day_movement_prediction.py
@@ -63,14 +63,17 @@ def theta_regularizer(theta):
     row_sums = torch.sum(theta, dim=-1)
     ones = torch.ones_like(row_sums)
     return torch.sum(torch.abs(row_sums - ones))
-            
+
 def neighbor_distance_regularizer(theta):
     box = torch.sum(theta, dim=-1)
+    result = torch.zeros_like(theta)
+
     for idx, row in enumerate(theta):
         for i, j in enumerate(row):
-            theta[idx][i] = i * j
-    theta = torch.sum(theta, dim=1)
-    return theta / box
+            result[idx, i] = i * j
+
+    result_sum = torch.sum(result, dim=1)
+    return result / result_sum[:, None]
 
 optimizer = torch.optim.Adam(model.parameters(), lr=2e-4)
 
