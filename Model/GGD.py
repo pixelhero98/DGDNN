4c1560247c7e09e93923e968adb844a289842a4b
pixelhero98 <116365512+pixelhero98@users.noreply.github.com>
2024-07-11 17:06:28 +0100

    Update GGD.py

--- Model/GGD.py
+++ Model/GGD.py
@@ -2,22 +2,20 @@ import torch
 import torch.nn as nn
 
 class GeneralizedGraphDiffusion(torch.nn.Module):
-
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim, output_dim, active):
         super(GeneralizedGraphDiffusion, self).__init__()
         self.output = output_dim
-        self.fc_layer = nn.Linear(input_dim, output_dim)
+        self.fc = nn.Linear(input_dim, output_dim)
         self.activation0 = torch.nn.PReLU()
+        self.active = active
 
     def forward(self, theta, t, x, a):
-
-        q = torch.zeros_like(a)
-        h = x
-
+        q = 0
         for i in range(theta.shape[0]):
             q += theta[i] * t[i]
+        
+        x = self.fc((q * a) @ x)
+        if self.active:
+            x = self.activation0(x)
 
-        h = self.fc_layer((q * a) @ h)
-        h = self.activation0(h)
-
-        return h
+        return x
