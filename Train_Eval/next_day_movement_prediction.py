2f332a162a8ba682894197765d05f4af351824f7
pixelhero98 <116365512+pixelhero98@users.noreply.github.com>
2025-04-18 15:10:59 +0100

    Update next_day_movement_prediction.py

--- Train_Eval/next_day_movement_prediction.py
+++ Train_Eval/next_day_movement_prediction.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from graph_dataset_gen import Mydataset
+from dataset_gen import Mydataset
 from dgdnn import DGDNN
 from torch_geometric.logging import log
 import torch.distributions
