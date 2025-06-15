64e47a6b27e8c57c433ce5daf318ea96e50a4ea5
pixelhero98 <116365512+pixelhero98@users.noreply.github.com>
2025-06-15 13:43:00 +0100

    Update Model.py

--- Model/Model.py
+++ Model/Model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from GGD import GeneralizedGraphDiffusion
-from CatAttn import CatMultiAttn
+from CATATTN import CatMultiAttn
 
 
 class DGDNN(nn.Module):
